0085d83e5ff70c0bf5a7998ffaf96cc913206481
Manoj Kumar Pacha <manojkumarpacha@Manojs-MacBook-Air.local>
2022-10-09 14:37:16 -0400

    cehck in from Mac for kafka send

--- src/main/java/com/home/kafkapoc/resource/UserResource.java
+++ src/main/java/com/home/kafkapoc/resource/UserResource.java
@@ -1,7 +1,24 @@
 package com.home.kafkapoc.resource;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("kafka")
 public class UserResource {
+    @Autowired
+    private KafkaTemplate<String, String> kafkaTemplate;
+    private static final String TOPIC = "MyFirstTopic";
+    @GetMapping("/put/{message}")
+    public String post(@PathVariable("message") final String message1){
+
+        kafkaTemplate.send(TOPIC,message1);
+        System.out.println(message1);
+        return "Sent Message";
+    }
+
 }
